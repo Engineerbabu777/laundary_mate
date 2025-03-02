95b0242a473a9f58c85f4e329c2a727c64c9d06c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-02 17:42:02 +0500

    Tabs Layout and files added

---
+++ app/(tabs)/basket/index.tsx
@@ -0,0 +1,18 @@
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Basket = (props: Props) => {
+  return (
+    <View>
+      <Text>Basket</Text>
+    </View>
+  )
+}
+
+export default Basket
+
+const styles = StyleSheet.create({})
